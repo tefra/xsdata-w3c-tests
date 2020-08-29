c99a6fe5a971a084589ed8a8667ab388b96af113
Chris <chris@komposta.net>
2020-08-29 13:21:10 +0300

    Removed leftovers

--- output/models/sun_data/stype/st_target_ns/st_target_ns00101m/st_target_ns00101m_xsd/__init__.py
+++ output/models/sun_data/stype/st_target_ns/st_target_ns00101m/st_target_ns00101m_xsd/__init__.py
@@ -1,7 +1,1 @@
 from output.models.sun_data.stype.st_target_ns.st_target_ns00101m.st_target_ns00101m_xsd.st_target_ns00101m import Test
-from output.models.sun_data.stype.st_target_ns.st_target_ns00101m.st_target_ns00101m_xsd.xsi import (
-    Nil,
-    NoNamespaceSchemaLocation,
-    SchemaLocation,
-    Type,
-)
--- output/models/sun_data/stype/st_target_ns/st_target_ns00201m/st_target_ns00201m_xsd/__init__.py
+++ output/models/sun_data/stype/st_target_ns/st_target_ns00201m/st_target_ns00201m_xsd/__init__.py
@@ -1,7 +1,1 @@
 from output.models.sun_data.stype.st_target_ns.st_target_ns00201m.st_target_ns00201m_xsd.st_target_ns00201m import Test
-from output.models.sun_data.stype.st_target_ns.st_target_ns00201m.st_target_ns00201m_xsd.xsi import (
-    Nil,
-    NoNamespaceSchemaLocation,
-    SchemaLocation,
-    Type,
-)

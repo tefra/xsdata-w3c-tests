23bc0fa5f0e3fe9f0c50df8d4d9369568bbc79de
Christodoulos Tsoulloftas <chris@komposta.net>
2023-02-23 12:29:55 +0200

    Update xsdata

--- output/instances/saxonData/Open/open027.v1.py
+++ output/instances/saxonData/Open/open027.v1.py
@@ -1,12 +1,12 @@
-from output.models.saxon_data.open.open027_xsd.open027 import Doc
+from output.models.saxon_data.open.open031_xsd.open031 import Doc
 
 
 obj = Doc(
+    open_com_element=None,
     a=[
         "",
     ],
     b="",
     c="",
-    d="",
-    open_com_element=None
+    d=""
 )
